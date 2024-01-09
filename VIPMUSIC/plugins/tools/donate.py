5b6b8b358293fb5564e01d328abe49b37c341ac4
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-09 17:18:12 +0530

    Create donate.py

---
+++ VIPMUSIC/plugins/tools/donate.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from VIPMUSIC import app
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+app.on_message(
+    filters.command("donate")
+    & filters.group)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/403d1431dd35d74d6fcad.jpg",
+        caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐅𝐨𝐫 𝐃𝐨𝐧𝐚𝐭𝐞 🥀""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        " 𝐃ᴏɴᴀᴛᴇ ", url=f"https://t.me/II_BAD_MUNDA_II")
+                ]
+            ]
+        ),
+    )
+
