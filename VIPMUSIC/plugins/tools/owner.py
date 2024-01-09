4f761de72865a57e9c3534702555ce79129c3e21
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-09 12:35:24 +0530

    Update owner.py

--- VIPMUSIC/plugins/tools/owner.py
+++ VIPMUSIC/plugins/tools/owner.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     & ~filters.edited & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/f73af9a4ffe130a83d8d2.jpg",
+        photo=f"https://telegra.ph/file/403d1431dd35d74d6fcad.jpg",
         caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐅𝐨𝐫 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 🥀""",
         reply_markup=InlineKeyboardMarkup(
             [
