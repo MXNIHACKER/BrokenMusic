b609e9b15a1fded35125f428cf4518b0980c2c5c
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-09 21:42:29 +0530

    Update badban.py

--- VIPMUSIC/plugins/sudo/badban.py
+++ VIPMUSIC/plugins/sudo/badban.py
@@ -4,7 +4,7 @@ from VIPMUSIC import app, userbot
 from VIPMUSIC.misc import SUDOERS
 from pyrogram import * 
 from pyrogram.types import *
-from VIPMUSIC.utils.vip_ban import admin_filter
+from VIPMUSIC.utils.bad_ban import admin_filter
 
 
 
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["roken"], prefixes=["B", "b"]) & admin_filter)
+@app.on_message(filters.command(["roken"], prefixes=["b", "B"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
