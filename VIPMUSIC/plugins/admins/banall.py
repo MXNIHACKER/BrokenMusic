135b773daf9cd26f24f754ff30f35e5e07b0d896
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-09 21:37:04 +0530

    Update banall.py

--- VIPMUSIC/plugins/admins/banall.py
+++ VIPMUSIC/plugins/admins/banall.py
@@ -2,7 +2,7 @@ from VIPMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from VIPMUSIC.utils.vip_ban import admin_filter
+from VIPMUSIC.utils.bad_ban import admin_filter
 
 
 
