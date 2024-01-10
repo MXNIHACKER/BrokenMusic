4f5c03656ef47002ed4ada343f67a8359ee244a5
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-10 15:42:18 +0530

    Update badupdate.py

--- VIPMUSIC/plugins/tools/badupdate.py
+++ VIPMUSIC/plugins/tools/badupdate.py
@@ -40,7 +40,7 @@ async def help(client: Client, message: Message):
     )
 
 @app.on_message(
-    filters.command("source")
+    filters.command("source", "repo")
     & filters.group)
 async def help(client: Client, message: Message):
     await message.reply_photo(
