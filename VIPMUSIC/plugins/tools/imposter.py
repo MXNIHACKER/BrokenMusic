e5ba1f7714dee8a1d371b012848cb76acac0379f
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-10 11:39:14 +0530

    Update imposter.py

--- VIPMUSIC/plugins/tools/imposter.py
+++ VIPMUSIC/plugins/tools/imposter.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from VIPMUSIC.plugins.tools.pretenderdb.py import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
+from VIPMUSIC.plugins.tools.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
 from VIPMUSIC import app
 
 
