25807dfba272c6f80acaa6d0caccc57d2ce2160f
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-15 23:08:23 +0530

    Update utag.py

--- VIPMUSIC/plugins/admins/utag.py
+++ VIPMUSIC/plugins/admins/utag.py
@@ -20,7 +20,7 @@ async def tag_all_users(_,message):
             if message.chat.id not in SPAM_CHATS:
                 break       
             usernum += 5
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"\n🙈 sᴜɴᴏ ɴᴀ ❤️ [{m.user.first_name}](tg://user?id={m.user.id})\n"
             if usernum == 1:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(2)
@@ -40,7 +40,7 @@ async def tag_all_users(_,message):
             if message.chat.id not in SPAM_CHATS:
                 break 
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"\n🙈 sᴜɴᴏ ɴᴀ ❤️ [{m.user.first_name}](tg://user?id={m.user.id})\n"
             if usernum == 5:
                 await app.send_message(message.chat.id,f'{text}\n{usertxt}')
                 await asyncio.sleep(2)
