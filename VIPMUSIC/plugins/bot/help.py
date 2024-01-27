1054a20111ae20fe02757f9e17d672be84c57968
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-27 14:57:16 +0530

    Update help.py

--- VIPMUSIC/plugins/bot/help.py
+++ VIPMUSIC/plugins/bot/help.py
@@ -61,7 +61,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                   "😎𝗣𝗔𝗛𝗟𝗘 𓆩𝗩𝗜𝗣𓆪 𝗞𝗢 𝗣𝗔𝗣𝗔 𝗕𝗢𝗟 𝗝𝗔𝗞𝗘 😆😆", show_alert=True
+                   "ᴘʜᴇʟᴇ 🕊️⃝‌ٖٖٖٖ ‌ٖٖٖٖٖ𝐁α∂ ❤️ᥫ᭡፝֟፝֟ ᴋᴏ ᴘᴀᴘᴀ ʙᴏʟ ᴊᴀᴋᴅ 😆😆", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
