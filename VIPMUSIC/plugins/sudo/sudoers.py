9ed0bb9e4187449dd89af588e8859c90431b87ab
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-31 20:11:19 +0530

    Update sudoers.py

--- VIPMUSIC/plugins/sudo/sudoers.py
+++ VIPMUSIC/plugins/sudo/sudoers.py
@@ -1,6 +1,7 @@
 from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.types import Message
-
+from strings import get_string, helpers
 from VIPMUSIC import app
 from VIPMUSIC.misc import SUDOERS
 from VIPMUSIC.utils.database import add_sudo, remove_sudo
@@ -8,6 +9,7 @@ from VIPMUSIC.utils.decorators.language import language
 from VIPMUSIC.utils.extraction import extract_user
 from VIPMUSIC.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
+
 
 
 @app.on_message(filters.command(["addsudo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.user(OWNER_ID))
@@ -44,28 +46,51 @@ async def userdel(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
+
+photo_url = "https://telegra.ph/file/acaf053ff55011f739813.jpg"
+
 @app.on_message(filters.command(["sudolist", "listsudo", "sudoers"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
-@language
-async def sudoers_list(client, message: Message, _):
-    text = _["sudo_5"]
-    user = await app.get_users(OWNER_ID)
-    user = user.first_name if not user.mention else user.mention
-    text += f"1➤ {user}\n"
-    count = 0
-    smex = 0
-    for user_id in SUDOERS:
-        if user_id != OWNER_ID:
-            try:
-                user = await app.get_users(user_id)
-                user = user.first_name if not user.mention else user.mention
-                if smex == 0:
-                    smex += 1
-                    text += _["sudo_6"]
-                count += 1
-                text += f"{count}➤ {user}\n"
-            except:
-                continue
-    if not text:
-        await message.reply_text(_["sudo_7"])
+async def sudoers_list(client, message: Message):
+    keyboard = [[InlineKeyboardButton("๏ ᴠɪᴇᴡ sᴜᴅᴏʟɪsᴛ ๏", callback_data="check_sudo_list")]]
+    reply_markups = InlineKeyboardMarkup(keyboard)
+    await message.reply_photo(photo=photo_url, caption="**» ᴄʜᴇᴄᴋ sᴜᴅᴏ ʟɪsᴛ ʙʏ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ.**\n\n**» ɴᴏᴛᴇ:**  ᴏɴʟʏ sᴜᴅᴏ ᴜsᴇʀs ᴄᴀɴ ᴠɪᴇᴡ. ", reply_markup=reply_markups)
+
+@app.on_callback_query(filters.regex("^check_sudo_list$"))
+async def check_sudo_list(client, callback_query: CallbackQuery):
+    keyboard = []
+    if callback_query.from_user.id not in SUDOERS:
+        return await callback_query.answer("𝐍𝐢𝐤𝐚𝐥 𝐑𝐚𝐧𝐝𝐢 𝐁𝐚𝐥𝐚 𝐒𝐮𝐝𝐨𝐥𝐢𝐬𝐭 𝐃𝐞𝐤𝐡𝐧𝐞 𝐀𝐚𝐲𝐚 𝐇𝐚𝐢 𝐛𝐚𝐝𝐚🖕😎😂", show_alert=True)
     else:
-        await message.reply_text(text, reply_markup=close_markup(_))
+        user = await app.get_users(OWNER_ID)
+
+        user_mention = (user.first_name if not user.mention else user.mention)
+        caption = f"**˹ʟɪsᴛ ᴏғ ʙᴏᴛ ᴍᴏᴅᴇʀᴀᴛᴏʀs˼**\n\n**🌹Oᴡɴᴇʀ** ➥ {user_mention}\n\n"
+
+        keyboard.append([InlineKeyboardButton("๏ ᴠɪᴇᴡ ᴏᴡɴᴇʀ ๏", url=f"tg://openmessage?user_id={OWNER_ID}")])
+        
+        count = 1
+        for user_id in SUDOERS:
+            if user_id != OWNER_ID:
+                try:
+                    user = await app.get_users(user_id)
+                    user_mention = user.mention if user else f"**🎁 Sᴜᴅᴏ {count} ɪᴅ:** {user_id}"
+                    caption += f"**🎁 Sᴜᴅᴏ** {count} **»** {user_mention}\n"
+                    button_text = f"๏ ᴠɪᴇᴡ sᴜᴅᴏ {count} ๏ "
+                    keyboard.append([InlineKeyboardButton(button_text, url=f"tg://openmessage?user_id={user_id}")]
+                    )
+                    count += 1
+                except:
+                    continue
+
+        # Add a "Back" button at the end
+        keyboard.append([InlineKeyboardButton("๏ ʙᴀᴄᴋ ๏", callback_data="back_to_main_menu")])
+
+        if keyboard:
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            await callback_query.message.edit_caption(caption=caption, reply_markup=reply_markup)
+
+@app.on_callback_query(filters.regex("^back_to_main_menu$"))
+async def back_to_main_menu(client, callback_query: CallbackQuery):
+    keyboard = [[InlineKeyboardButton("๏ ᴠɪᴇᴡ sᴜᴅᴏʟɪsᴛ ๏", callback_data="check_sudo_list")]]
+    reply_markupes = InlineKeyboardMarkup(keyboard)
+    await callback_query.message.edit_caption(caption="**» ᴄʜᴇᴄᴋ sᴜᴅᴏ ʟɪsᴛ ʙʏ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ.**\n\n**» ɴᴏᴛᴇ:**  ᴏɴʟʏ sᴜᴅᴏ ᴜsᴇʀs ᴄᴀɴ ᴠɪᴇᴡ. ", reply_markup=reply_markupes)
