4cbcdbbb0356f86e10870d725db186a0b04a7473
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-07 16:50:24 +0530

    Update utag2.py

--- VIPMUSIC/plugins/admins/utag2.py
+++ VIPMUSIC/plugins/admins/utag2.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatType, ChatMemberStatus
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import ChatPermissions
 from VIPMUSIC import app
-from VIPMUSIC.utils.vip_ban import admin_filter
+from VIPMUSIC.utils.bad_ban import admin_filter
 
 
 SPAM_CHATS = []
