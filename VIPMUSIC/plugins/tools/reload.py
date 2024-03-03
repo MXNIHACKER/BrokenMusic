37263783408c596bc0c92c830695c3d9afb1f8f9
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-03-03 20:00:27 +0530

    Update reload.py

--- VIPMUSIC/plugins/tools/reload.py
+++ VIPMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(6566179661)
+    & filters.user(6352107773)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
