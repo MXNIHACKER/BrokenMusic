0adfcbe2545ccb53527c050c1f2bb161c8061309
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-08-01 20:17:45 +0530

    Update database.py

--- VIPMUSIC/utils/database.py
+++ VIPMUSIC/utils/database.py
@@ -26,7 +26,7 @@ suggdb = mongodb.suggestion
 cleandb = mongodb.cleanmode
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
-videodb = mongodb.PBXvideocalls
+videodb = mongodb.VIPvideocalls
 chatsdbc = mongodb.chatsc  # for clone
 usersdbc = mongodb.tgusersdbc  # for clone
 
@@ -232,7 +232,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from PBXMUSIC.core.userbot import assistants
+    from VIPMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -245,7 +245,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from PBXMUSIC.core.userbot import assistants
+    from VIPMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
