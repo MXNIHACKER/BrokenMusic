33fe0fcbba1b11c4f8cf60554d0894ec5c52e7a8
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-08-01 20:23:30 +0530

    Update Youtube.py

--- VIPMUSIC/platforms/Youtube.py
+++ VIPMUSIC/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from DAXXMUSIC.utils.database import is_on_off
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from VIPMUSIC.utils.database import is_on_off
+from VIPMUSIC.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
